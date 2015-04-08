73d174868b2bcf5cee3cf3a939bed6ffd00888c0
Oscar Jonsson <ogggan@gmail.com>
2015-04-09 00:44:15 +0200

    Made changes to the way the json-data is displayed by hicurl.js Now the json-panel(jstree) shows the entire data. And clicking on pages in the page-panel selects the corresponding page-node in the json-panel.

--- src/hicurl-public/hicurl.js
+++ src/hicurl-public/hicurl.js
@@ -37,17 +37,37 @@ function Hicurl(domElement,dataUrl) {
 
 Hicurl.prototype._historyLoadedHandler=function(data){
 	var pages=(this._data=data).pages;
-	var treeData=[];
+	var treeData=[{text:"Root",children:[]}];
 	for (var i=0; i<pages.length; i++) {
-		var treeItem=treeData[i]={};
+		var treeItem=treeData[0].children[i]={};
 		var page=pages[i];
 		treeItem.text=page.name||"Page "+(i+1);
 		treeItem.index=i;
 	}
 	$(this._pageTree).tree({
+		lines:true,
 		data:treeData,
 		onClick:$.proxy(this._pageClick,this)
 	});
+	
+	
+	var dataClone=JSON.parse(JSON.stringify(this._data));
+	var numPages=dataClone.pages.length;
+	for (var i=0; i<numPages; i++) {
+		var pageCloneExchanges=dataClone.pages[i].exchanges;
+		for (var j=0; j<pageCloneExchanges.length; j++) {
+			pageCloneExchanges[j].content="&lt;content&gt;";
+		}
+	}
+	dataClone=Hicurl._jstreeFormat(dataClone,"Root");
+	for (var i=0; i<numPages; i++) {
+		dataClone.children[0].children[i].id="hicurl_jstree_page_"+i;
+	}
+	dataClone.id="hicurl_jstree_page_root";
+	$(this._jsonPanel).jstree({
+	core : {
+		data:dataClone
+	} });
 	
 	//this has to be called otherwise the layout will "sometimes" be a little messed up after the writing to the
 	//pageTree. calling it without the setTimeout doesn't fix it either, at least not always.
@@ -55,32 +75,31 @@ Hicurl.prototype._historyLoadedHandler=function(data){
 	setTimeout($.parser.parse);
 };
 Hicurl.prototype._pageClick=function(node) {
+	var rootNode=$(this._jsonPanel).jstree("select_node", 1); 
+	
+	$(this._jsonPanel).jstree("deselect_all");
 	//remove all content-tabs(except from the very left one with the title of "Content", which actuallt is a tab too
 	while ($(this._contentTabs).tabs('getTab',1))
 		$(this._contentTabs).tabs('close',1);
 	
-	var pageDataClone=JSON.parse(JSON.stringify(this._data.pages[node.index]));
-	for (var i=0; i<pageDataClone.exchanges.length; i++) {
-		pageDataClone.exchanges[i].content="&lt;content&gt;";
+	if (node.index==null) {
+		$(this._jsonPanel).jstree("select_node","hicurl_jstree_page_root"); 
+	} else {
+		//tabs don't work in textareas with nowrap-setting(whitespace in general is limited)
+		var opts={indent_char:" "};//...so using non-breaking-space instead
+		$(this._jsonPanel).jstree("select_node","hicurl_jstree_page_"+node.index); 
+		var exchanges=this._data.pages[node.index].exchanges;
+		for (var i=0; i<exchanges.length; i++) {
+			var textArea=document.createElement("textArea");
+			textArea.value=exchanges[i].content,opts;//textArea.value=html_beautify(exchanges[i].content,opts);
+			textArea.readOnly = true;
+			$(this._contentTabs).tabs('add',{
+				title: !exchanges[i].error?"Success":exchanges[i].error.split("\n")[0],
+				selected: true,
+				content:textArea,
+			});
+		}
 	}
-	//tabs don't work in textareas with nowrap-setting(whitespace in general is limited)
-	var opts={indent_char:" "};//...so using non-breaking-space instead
-$(this._jsonPanel).jstree({
-	core : {
-		data:Hicurl._jstreeFormat(pageDataClone,node.text)
-	} });
-	var exchanges=this._data.pages[node.index].exchanges;
-	for (var i=0; i<exchanges.length; i++) {
-		var textArea=document.createElement("textArea");
-		textArea.value=exchanges[i].content,opts;//textArea.value=html_beautify(exchanges[i].content,opts);
-		textArea.readOnly = true;
-		$(this._contentTabs).tabs('add',{
-			title: !exchanges[i].error?"Success":exchanges[i].error.split("\n")[0],
-			selected: true,
-			content:textArea,
-		});
-	}
-	
 };
 
 
