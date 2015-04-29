6bdc76291234b9c02ea62058172fd66bc26ce150
Oscar Jonsson <ogggan@gmail.com>
2015-04-29 17:55:14 +0200

    History is now stored in separate files.

--- testing/multiLoadSingles.php
+++ testing/multiLoadSingles.php
@@ -1,8 +1,7 @@
 <?php
 require_once '../src/hicurl.php';
-unlink('data/history.tmp');
-$hicurl=new Hicurl(['history'=>'data/history.tmp']);
+$hicurl=new Hicurl(['history'=>'data/history']);
 $hicurl->loadSingle('www.google.com',null,null,['name'=>'Google']);
 $hicurl->loadSingle('www.facebook.com',null,null,['name'=>'Facebook']);
-$hicurl->compileHistory('data/history.gz');
+//$hicurl->compileHistory('data/history.gz');
 include "assets/test.htm";
