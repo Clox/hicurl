abd0f10f4b2ba9fc1c949f37b32e72698ec05adb
Oscar Jonsson <ogggan@gmail.com>
2020-04-11 22:10:04 +0200

    Added setting for jsonPayload. Version now at 3.0.0 because getheaders and postheaders and been changed from being added as indexed arrays with each element being like "$key:$value" with having an associative array instead.

--- testing/loadSingleWithInstance.php
+++ testing/loadSingleWithInstance.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../src/hicurl.php';
 unlink('data/history.tmp');
-$hicurl=new Hicurl(['history'=>'data/history.tmp']);
+$hicurl=new Hicurl(['history'=>'data/history']);
 $hicurl->loadSingle('www.google.com');
-$hicurl->compileHistory('data/history.gz');
-include "assets/test.htm";
+//$hicurl->compileHistory('data/history.gz');
+//include "assets/test.htm";
